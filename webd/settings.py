b1ec1ed327698f140a712f7587e6ce0df3918797
nick <nickas101@gmail.com>
2020-03-30 15:09:53 +1300

    applications added to settings

--- webd/settings.py
+++ webd/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lomas.apps.LomasConfig',
+    'lta.apps.LtaConfig',
+    'process.apps.ProcessConfig',
 ]
 
 MIDDLEWARE = [
